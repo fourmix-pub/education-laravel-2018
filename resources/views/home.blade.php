25da3dbe87ab80919e583adf1a276b35092163ad
永保彩佳 <nagahoayaka@anagahofourmix.lan>
2018-07-12 11:06:02 +0900

    home画面作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,103 @@
-@extends('layouts.app')
+!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">メニュー</div>
+<main class="py-4">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <h1 class="display-2">「話のタネ」を育てよう。</h1>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <a href="{{ route('articles.create') }}" class="btn btn-primary btn-lg btn-block">
-                        記事作成
-                    </a>
-                    <a href="{{ route('articles.index') }}" class="btn btn-warning btn-lg btn-block">
-                        記事一覧
-                    </a>
-                    <a href="#" class="btn btn-info btn-lg btn-block">
-                        下書き
-                    </a>
-                </div>
+                <h2>
+                    「創作ノート」は、思いついたストーリーの「5W1H」を記録することで<br>
+                    「話のタネ」が「物語」になるのを応援するサイトです。
+                  </h2>
+                <br>
+                <br>
             </div>
         </div>
+        <a href="login.html" class="btn btn-outline-primary btn-block ">ログイン</a>
+        <br>
+        <a href="signup.html" class="btn btn-outline-info btn-block">新規登録</a>
+        <img src="img/me.png" class="rounded float-right" alt="growup">
+
     </div>
+</main>
 </div>
 @endsection
