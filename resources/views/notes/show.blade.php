bda3d39d50436574a9255ef9957e084ab4502e11
永保彩佳 <nagahoayaka@anagahofourmix.lan>
2018-07-18 16:52:28 +0900

    更新

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -10,11 +10,6 @@
                 </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
                     いつ：{{ $note->when }}<br>
                     どこで：{{ $note->where }}<br>
                     誰が：{{ $note->who }}<br>
